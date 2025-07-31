ca6bab09575a3eb3acbabae4075f4c7e5779f898
clydexmarty09 <cmarty@ucsd.edu>
2025-07-31 09:53:08 -0700

    Added simple style to first card

--- js/javascript.js
+++ js/javascript.js
@@ -4,11 +4,24 @@ class artCard extends HTMLElement {
 
         const shadow = this.attachShadow({mode:'open'}); 
         const img = document.createElement('img'); 
+        const style = document.createElement('style'); 
         img.src = "assets/gallery-pictures/webp/ghost-min.webp"; 
+
+        style.textContent = `
+
+            img {
+                margin:1rem; 
+                padding:0.5rem; 
+                height: 14rem;
+                width:11rem;  
+                outline: solid white; 
+                display:flex; flex-direction:row; 
+            }
         
+        `; 
 
         shadow.appendChild(img); 
-        
+        shadow.appendChild(style); 
     }
     // function here
 }
