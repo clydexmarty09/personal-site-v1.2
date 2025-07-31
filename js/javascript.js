bd2bf14a5abcac11c70009f4fdd07476972a0a3f
clydexmarty09 <cmarty@ucsd.edu>
2025-07-30 21:06:09 -0700

    added some elements in JS

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,11 @@
 class artCard extends HTMLElement {
     constructor() {
         super(); 
+
+        const name = document.createElement('h2'); 
+        const picture = document.createElement('picture'); 
+        const link = document.createElement('a'); 
+        const p = document.createElement('p'); // short description 
     }
     // function here
 }
