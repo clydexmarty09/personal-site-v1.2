94ef0a12226ea9962b1dca19939b40e09e6e58cf
clydexmarty09 <cmarty@ucsd.edu>
2025-07-30 23:51:34 -0700

    starting this entire class over

--- js/javascript.js
+++ js/javascript.js
@@ -2,10 +2,7 @@ class artCard extends HTMLElement {
     constructor() {
         super(); 
 
-        const name = document.createElement('h2'); 
-        const picture = document.createElement('picture'); 
-        const link = document.createElement('a'); 
-        const p = document.createElement('p'); // short description 
+        
     }
     // function here
 }
