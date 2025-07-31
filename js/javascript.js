82d61b01cee8f0e83f51d844f22282ba88405705
clydexmarty09 <cmarty@ucsd.edu>
2025-07-31 09:46:44 -0700

    manage to display first image

--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,12 @@ class artCard extends HTMLElement {
     constructor() {
         super(); 
 
+        const shadow = this.attachShadow({mode:'open'}); 
+        const img = document.createElement('img'); 
+        img.src = "assets/gallery-pictures/webp/ghost-min.webp"; 
+        
+
+        shadow.appendChild(img); 
         
     }
     // function here
